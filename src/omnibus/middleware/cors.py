5f17c5d91f39ba9b9d04c8b2134037c1a524ba6b
Haseeb Majid <hello@haseebmajid.dev>
2022-08-19 19:27:39 +0100

    test

--- src/omnibus/middleware/cors.py
+++ src/omnibus/middleware/cors.py
@@ -6,7 +6,7 @@ from pydantic import AnyHttpUrl
 def add_cors(app: FastAPI, cors: list[AnyHttpUrl], regex_cors: list[str] | None = None) -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=cors,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
