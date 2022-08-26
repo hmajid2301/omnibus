a1f9a05e63478a1dbaf059a66f387331fcd1c777
Haseeb Majid <haseeb@joinzoe.com>
2022-08-26 20:24:07 +0100

    test

--- src/omnibus/app.py
+++ src/omnibus/app.py
@@ -2,6 +2,7 @@ from typing import Any, Callable, Dict, List, Type, Union
 
 from beanie import init_beanie
 from beanie.odm.documents import DocType
+from beanie.odm.views import View
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_health import health
@@ -15,14 +16,19 @@ from omnibus.log.logger import get_logger, setup_logger
 async def setup_app(
     app: FastAPI,
     get_settings: Callable[..., OmnibusSettings],
-    document_models: List[Union[Type["DocType"], str]],
+    document_models: List[Union[Type["DocType"], Type["View"], str]],
     healthcheck: Callable[..., Union[Dict[str, Any], bool]] = default_healthcheck,
 ):
+    log = get_logger()
+    log.info("HERE")
     config = get_settings()
     setup_logger(log_level=config.LOG_LEVEL, env=config.ENVIRONMENT, uvicorn_log_level=config.UVICORN_LOG_LEVEL)
+    log.info("HERE11")
     uri = config.get_mongodb_uri()
     client = motor_asyncio.AsyncIOMotorClient(uri)
+    log.info("HERE12")
     await init_beanie(database=client[config.DB_NAME], document_models=document_models)
+    log.info("HERE13")
 
     app.add_middleware(
         CORSMiddleware,
@@ -31,9 +37,9 @@ async def setup_app(
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    log.info("HERE14")
 
     # add_cors(app=app)
     app.add_api_route("/health", health([healthcheck]))
 
-    log = get_logger()
     log.info(f"starting {app.title} {config.WEB_HOST}:{config.WEB_PORT}")
